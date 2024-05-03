153b0c2ed7043f114039c3a9019d4a29a90462af
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-03 05:53:21 +0530

    Delete services/GeneralSettings.py
    to update 0.0.4

--- services/GeneralSettings.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-
-class GeneralSettings:
-    general_settings: Dict = None
-
-    @staticmethod
-    def configure_general_settings(settings: Dict):
-        GeneralSettings.general_settings = settings
