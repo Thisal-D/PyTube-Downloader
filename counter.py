fbf79302d917912ecf5c09c76c1b6855f7171be7
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-03 07:58:30 +0530

    Delete counter.py

--- counter.py
+++
@@ -1,9 +0,0 @@
-from utils import ImageUtility
-from PIL import Image
-
-image_path = "assets\\profile images\\https~~~github.com~Thisal-D-normal.png"
-
-ImageUtility.create_image_with_hover_effect(
-    image=Image.open(image_path),
-    intensity_increase=50
-).save("2.png")
