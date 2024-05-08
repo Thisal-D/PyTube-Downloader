aa54fe2b96947e1251df6e6211eb509892f2103a
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-09 03:45:27 +0530

    Delete functions directory

--- functions/convertTime.py
+++
@@ -1,9 +0,0 @@
-def convertTime(s: int) -> str:
-    h = int(s/3600)
-    m = int((s - (h*3600)) / 60)
-    s = s - (h*3600) - (m*60)
-    if h>0:
-        converted_time = f"{h}:{m:0>2}:{s:0>2}"
-    else:
-        converted_time = f"{m}:{s:0>2}"
-    return converted_time
--- functions/removeInvalidCharts.py
+++
@@ -1,6 +0,0 @@
-def removeInvalidChars(url: str) -> str:
-    filename = url
-    replaces = ["\\", "/", ":", '"', "?", "<", ">", "|", "*"]
-    for re in replaces:
-        filename = filename.replace(re, "~")
-    return filename
