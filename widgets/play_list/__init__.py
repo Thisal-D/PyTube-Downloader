986f1e7f092f756aba795dac2be1e4e03f009f2e
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-09 04:16:55 +0530

    Delete widgets directory

--- widgets/play_list/__init__.py
+++
@@ -1,4 +0,0 @@
-from .play_list import PlayList
-from .added_play_list import AddedPlayList
-from .downloading_play_list import DownloadingPlayList
-from .downloaded_play_list import DownloadedPlayList
--- widgets/video/__init__.py
+++
@@ -1,4 +0,0 @@
-from .video import Video
-from .added_video import AddedVideo
-from .downloading_video import DownloadingVideo
-from .downloaded_video import DownloadedVideo
