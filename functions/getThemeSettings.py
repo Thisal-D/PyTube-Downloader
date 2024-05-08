425d41b77c9e89aa9fd5a2a8eaf9ccad09b389ce
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-09 02:54:09 +0530

    Delete functions directory

--- functions/getThemeSettings.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def getThemeSettings():
-    file_name = ".\\settings\\theme.json"
-    settings  = json.load(open(file_name, "r"))
-    for key in settings.keys():
-        if type(settings[key]) == list:
-            settings[key] = tuple(settings[key])
-    return settings
