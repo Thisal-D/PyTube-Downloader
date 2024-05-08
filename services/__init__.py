10b463d693bce51cdc917ae584ba0e8e11727547
Thisal Dilmith <93121062+Thisal-D@users.noreply.github.com>
2024-05-09 02:25:56 +0530

    Delete services directory

--- services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .loading_indicate_manager import LoadingIndicateManager
-from .load_manager import LoadManager
-from .download_manager import DownloadManager
-from .theme_manager import ThemeManager
